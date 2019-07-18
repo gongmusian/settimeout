baa57c94f50eadbbdc263b1b8619745049459c5e
Robert Kung  龔慕賢 [IP] <Robert.Kung@iprospect.com>
2019-07-18 17:26:45 +0800

    first comiit

---
+++ customjs.js
@@ -0,0 +1,11 @@
+ //JavaScript Document
+
+$(document).ready(function(){
+  $("#btn1").click(function(){
+    $("p").append(" <b>Appended text</b>.");
+  });
+
+  $("#btn2").click(function(){
+    $("ol").append("<li>Appended item</li>");
+  });
+});
